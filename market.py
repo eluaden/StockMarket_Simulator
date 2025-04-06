a1506995af4075f28df50782b58cfd0d5f504c02
Melhor <39880896+mudavel@users.noreply.github.com>
2025-04-06 17:47:38 -0300

    time_manager: add method is_market_open(), change method advance_day()

--- market.py
+++ market.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 from action import Action
 from action_traits import ActionTraits
 from name_generator import NameGenerator
@@ -22,7 +23,8 @@ class Market:
             size (int): The number of actions to generate in the market.
         """
         self.actions = self.generate_actions(size)
-        self.time_manager = TimeManager()
+        self.time_manager = TimeManager(
+            datetime.time(10, 0), datetime.time(18, 0))
 
     def generate_actions(self, size):
         """
@@ -42,15 +44,17 @@ class Market:
             action_shares = random.randint(50000, 10000000)
             action_level = random.randint(1, 4)
             action_traits = ActionTraits()
-            
-            #generate a name without repeating it
+
+            # generate a name without repeating it
             while True:
-                action_name = NameGenerator.generate_name(action_traits.major_sector)
+                action_name = NameGenerator.generate_name(
+                    action_traits.major_sector)
                 action_ticker = NameGenerator.action_ticker(action_name)
                 if action_ticker not in actions:
                     break
 
-            action_obj = Action(action_name, action_ticker,action_traits ,action_price, action_shares, action_level)
+            action_obj = Action(action_name, action_ticker, action_traits,
+                                action_price, action_shares, action_level)
 
             # Store the action object and its shares in the dictionary
             actions[action_obj.ticker] = [action_obj, action_shares]
